b952646866d74c4546abd371748e91f3db30df42
Joe Hitchens <joe@sleepless.com>
2012-01-28 19:45:25 -0800

    -

--- docroot/db.js
+++ docroot/db.js
@@ -3,25 +3,19 @@
 db = {}
 
 db.set = function(k, v, cb) {
-	RPC.send([
-		{ cmd : "set", key : k, value : v },
-	] , function(response) {
+	RPC.send("set", k, v, function(response) {
 		alert(JSON.stringify(response))
 	})
 }
 
 db.get = function(k, cb) {
-	RPC.send([
-		{ cmd : "get", key : k },
-	] , function(response) {
+	RPC.send("get", k, function(response) {
 		alert(JSON.stringify(response))
 	})
 }
 
 db.del = function(k, cb) {
-	RPC.send([
-		{ cmd : "del", key : k },
-	] , function(response) {
+	RPC.send("del", k, function(response) {
 		alert(JSON.stringify(response))
 	})
 }
