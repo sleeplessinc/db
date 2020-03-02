e79960892b89f3c7314b99f89eb310561ecb00a9
Joe Hitchens <joe@sleepless.com>
2020-03-01 20:31:05 -0800

    -

--- dynamodb.js
+++ dynamodb.js
@@ -9,34 +9,13 @@ AWS.config.loadFromPath('./aws_config.json');	// XXX
 exports.connect = function( opts ) {
 
 	let ddb = new AWS.DynamoDB( opts );
+	let dc = new AWS.DynamoDB.DocumentClient();
 
-	let tables = function( okay, fail ) {
-		ddb.listTables( ( error, data ) => {
-			if( error ) {
-				fail( error );
-			}
-			else {
-				okay( data );
-			}
-		});
+	let tables = function( cb ) {
+		ddb.listTables( cb );
 	};
 
-	/*
-	let insert = function( coll_name, obj, okay, fail ) {
-	};
-
-	let remove = function( coll_name, criteria, okay, fail ) {
-	};
-
-	let select = function( coll_name, criteria, okay, fail  ) {
-	};
-
-	let update = function( coll_name, obj, okay, fail ) {
-	};
-
-	*/
-
-	let createTable = function( table_name, okay, fail ) {
+	let createTable = function( table_name, cb ) {
 		var params = {
 			AttributeDefinitions: [
 				{ AttributeName: "id", AttributeType: "N" }, 
@@ -50,89 +29,25 @@ exports.connect = function( opts ) {
 			},
 			TableName: table_name
 		};
-		ddb.createTable( params, function( err, data ) {
-			if( err ) {
-				fail( err );
-			}
-			else {
-				okay( data );
-			}
-		});
+		ddb.createTable( params, cb );
 	}
 
-	let deleteTable = function( table_name, okay, fail ) {
+	let deleteTable = function( table_name, cb ) {
 		var params = {
 			TableName: table_name
 		};
-		ddb.deleteTable( params, function( err, data ) {
-			if( err ) {
-				fail( err );
-			}
-			else {
-				okay( data );
-			}
-		});
+		ddb.deleteTable( params, cb );
 	}
 
-	let putItem = function( table_name, data, okay, fail ) {
-
-		let item = {};
-		for( let k in data ) {
-			let val = data[ k ];
-			let type = typeof val;
-			log( type + " " + val );
-			switch( type ) {
-			case "string":
-				item[ k ] = { S: val }
-				break;
-			case "number":
-				item[ k ] = { N: (""+val) }
-				break;
-			case "object":
-				item[ k ] = { M: val }
-				break;
-			case "boolean":
-				item[ k ] = { B: val }
-				break;
-			}
-		}
-		log( "item=" + o2j(key));
+	let putItem = function( table_name, item, cb ) {
 		var params = {
 			Item: item,
 			TableName: table_name
 		};
-		ddb.putItem( params, function( err, data ) {
-			if( err ) {
-				fail( err );
-			}
-			else {
-				okay( data );
-			}
-		});
+		dc.put( params, cb );
 	}
 
-	let getItem = function( table_name, data, okay, fail ) {
-		let key = {};
-		for( let k in data ) {
-			let val = data[ k ];
-			let type = typeof val;
-			log( type + " " + val );
-			switch( type ) {
-			case "string":
-				key[ k ] = { S: val }
-				break;
-			case "number":
-				key[ k ] = { N: (""+val) }
-				break;
-			case "object":
-				key[ k ] = { M: val }
-				break;
-			case "boolean":
-				key[ k ] = { B: val }
-				break;
-			}
-		}
-		log( "key=" + o2j(key));
+	let getItem = function( table_name, key, cb ) {
 		let params  = {
 			TableName: table_name,
 			Key: key,
@@ -140,14 +55,7 @@ exports.connect = function( opts ) {
 			ConsistentRead: true,
 			//ReturnConsumedCapacity: "TOTAL"
 		}
-		ddb.getItem( params, function( err, data ) {
-			if( err ) {
-				fail( err );
-			}
-			else {
-				okay( data );
-			}
-		});
+		dc.get( params, cb );
 	}
 
 	return { 
@@ -165,19 +73,21 @@ if( require && require.main === module ) {
 
 	// this module is being executed directly so run some tests
 
-	function okay( a ) {
-		log( "OKAY: " + o2j(a) );
-	}
-	function fail( a ) {
-		console.warn( "FAIL: " + o2j(a) );
+	function fin( err, data ) {
+		if( err ) {
+			log( "ERROR: " + o2j( data ) );
+		}
+		else {
+			log( "OKAY: " + o2j( data ) );
+		}
 	}
 
 	db = require( "./index.js" ).dynamodb.connect( );
-	//db.tables( okay, fail );
-	//db.createTable( "foobs", okay, fail );
-	//db.deleteTable( "foobs", okay, fail );
-	//db.putItem( "users", { id: 3, name: "Briggs" }, okay, fail );
-	db.getItem( "users", { id: 3 }, okay, fail );
+	db.tables( fin );
+	//db.createTable( "foobs", fin );
+	//db.deleteTable( "foobs", fin );
+	//db.putItem( "users", { id: 4, name: "Sally" }, fin );
+	//db.getItem( "users", { id: 4 }, fin );
 
 
 }
